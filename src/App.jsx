bd4e45a9a598c1dc0131a87f31b3c94c7abf1978
RahulDhi123 <rahul.ee.25@nitj.ac.in>
2026-07-01 12:11:16 +0530

    Add Initial Files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from "./components/card.jsx"
+
+function App() {
+ return(
+<div className="parent">
+  <Card/>
+</div>
+
+ )
+}
+
+export default App
---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+
+import {Bookmark} from 'lucide-react'
+
+
+function Card(){
+   return(
+    <div className="card">
+        
+    </div>
+   )
+}
+
+export default Card
