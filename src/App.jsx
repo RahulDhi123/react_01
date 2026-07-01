4b3913036cc717d3d15fdc8c275a1d3c08fe126b
RahulDhi123 <rahul.ee.25@nitj.ac.in>
2026-07-01 14:42:22 +0530

    Adding Final Touch

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ import Card from "./components/card.jsx"
 function App() {
  return(
 <div className="parent">
-  <Card/>
+  <Card img="https://thumbs.dreamstime.com/b/icons-sample-psost-setting-428651105.jpg" company="Amazon"/>
+  <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIqBCl92-9ug8ad3PHtsqtNUKg7qhSw-zZBAyfDhu2rQ&s=10" company="Google"/>
+  <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx2xp09gxKvFPDzObUGYiX0gaFVIooCYNbi7ZMj_tQEQ&s" company="Uber"/>
+  <Card img="https://yt3.googleusercontent.com/btm1_PK-7VRUr9GY2D0UV_2XfbUZPBjghyptjSO1crsfN86HyTYDWPmUbq7JxC3H0Lxe_s067nA=s900-c-k-c0x00ffffff-no-rj" company="Nvidia"/>
+  <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyyTpdkQ3X-W9RF4UNmhP80r8w9jZ8NcMEFNP9NDm8wQ&s=10" company="Flipkart"/>
 </div>
 
  )
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,11 +2,42 @@
 import {Bookmark} from 'lucide-react'
 
 
-function Card(){
+function Card(props){
    return(
-    <div className="card">
-        
-    </div>
+   <div className="card">
+      <div className="top">
+        <img src={props.img} alt="" />
+        <button id="save">Save<Bookmark/></button>
+        </div>
+
+        <div className="mid">
+            <div className="one">
+                <span>{props.company}</span>
+                <span>5 days ago</span>
+            </div>
+
+            <div className="two">
+                <h2>Senior UI/UX Developer</h2>
+            </div>
+
+
+            <div className="three">
+                <button>Part Time</button>
+                <button>Senior Level</button>
+            </div>
+        </div>
+<hr />
+
+        <div className="bottom">
+            
+            <div className="info">
+                <p>$120/Hr</p>
+                <p>Mumbai,India</p>
+            </div>
+
+            <button > Apply </button>
+        </div>
+      </div>
    )
 }
 
